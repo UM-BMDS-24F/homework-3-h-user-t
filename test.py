5db73d5f52cce9816dd42a3fd3109597c301afd8
h-user-t <124530361+h-user-t@users.noreply.github.com>
2024-11-03 18:10:23 -0500

    deleting test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Add the BLAST+ bin directory to PATH
-blast_bin_dir = r"C:\Program Files\NCBI\blast-2.16.0+\bin"
-os.environ['PATH'] += os.pathsep + blast_bin_dir
-
-# Now print the PATH to verify
-print("Updated PATH environment variable in Python:")
-print(os.environ['PATH'])
